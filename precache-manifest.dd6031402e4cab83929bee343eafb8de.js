5d057543f3c6cbc61957015aae9f15f5ca065c68
Konstantin Akimov <konst.akimov@gmail.com>
2020-03-16 01:19:21 +0300

    Updates

---
+++ precache-manifest.dd6031402e4cab83929bee343eafb8de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7402fdb87a37afc9366437ecdc804e4c",
+    "url": "/image-gallery/index.html"
+  },
+  {
+    "revision": "e1cdd8e40709237e4bd8",
+    "url": "/image-gallery/static/css/main.acb691c2.chunk.css"
+  },
+  {
+    "revision": "0a06ece42a507c162a8c",
+    "url": "/image-gallery/static/js/2.fa7bf4ce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/image-gallery/static/js/2.fa7bf4ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1cdd8e40709237e4bd8",
+    "url": "/image-gallery/static/js/main.729d92ed.chunk.js"
+  },
+  {
+    "revision": "0e22ce4a4856e2c0018c",
+    "url": "/image-gallery/static/js/runtime-main.b0b4a87e.js"
+  }
+]);
